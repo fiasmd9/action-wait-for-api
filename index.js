67883522a94992712fe20a1272f1ccd8a315a29d
mohansaira.medisetti <mohansaira.medisetti@servicenow.com>
2022-01-12 12:45:04 +0530

    first test

--- index.js
+++ index.js
@@ -8,26 +8,16 @@ const { tryFetch } = require('./lib/try-fetch');
     let headersString = core.getInput('headers');
     let timeout = parseInt(core.getInput('timeout'));
     let interval = parseInt(core.getInput('interval'));
-    let expectedStatus = parseInt(core.getInput('expected-status'));
-    let expectedResponseField = core.getInput('expected-response-field');
-    let expectedResponseFieldValue = core.getInput(
-      'expected-response-field-value'
-    );
+    
 
     core.debug(`=== Waiting for API response to continue. ===`);
     core.debug(`URL: ${url}`);
     core.debug(`Method: ${method}`);
     core.debug(`Headers: ${headersString}`);
     core.debug('# Waiting for this response:');
-    core.debug(`HTTP Status: ${expectedStatus}`);
+    
 
-    if (expectedResponseField && expectedResponseFieldValue) {
-      core.debug(
-        `Response contains field "${expectedResponseField}" with value "${expectedResponseFieldValue}"`
-      );
-    } else if (expectedResponseField) {
-      core.debug(`Response contains field "${expectedResponseField}"`);
-    }
+   
 
     core.debug('');
     core.debug('');
@@ -41,10 +31,7 @@ const { tryFetch } = require('./lib/try-fetch');
       timeout,
       url,
       method,
-      headers,
-      expectedStatus,
-      expectedResponseField,
-      expectedResponseFieldValue,
+      headers
     });
 
     core.debug('API request was successfull.');
